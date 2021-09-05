8552bfd814eb440440e72abf78260eaf5db3c50b
Ignat Mora <morrig407@gmail.com>
2021-09-05 14:22:18 +0700

    Add open addresing w/ quadratic probing; Add item search

--- hashOpen.c
+++ hashOpen.c
@@ -1,4 +1,9 @@
-#define LINE "malanovromanigorevich"
+//#define LINE "malanovromanigorevich"
+#define LINE "qwertyuiopasdfghjklzxcvbnm"
+
+#ifndef TABLE
+#define TABLE 1
+#endif
 
 #include "hashOpenAddr.h"
 
@@ -13,10 +18,11 @@ void printTable(char* line, int size) {
 	putchar('\n');
 	for (int i = 0; i < size; ++i)
 		printf("%*c", n, line[i] ? line[i] : ' ');
+	putchar('\n');
 }
 
 int main() {
-	printf("Hashing2. Open addressing. Linear probing.\n");
+	printf("Hashing2. Open addressing. Linear and quadratic probing.\n");
 
 	int m;
 
@@ -29,12 +35,55 @@ int main() {
 		line[i] = (char) LINE[i];
 	for (int i = 0; i < length; ++i)
 		printf("%c", line[i]);
+	putchar('\n');
+	putchar('\n');
 
-	char *hashTable = hashOpenAddrLinear(line, m);
+	int coll;
+
+	printf("Linear probing:\n");
+	char *hashTable = hashOpenAddrLinear(line, m, &coll);
+	printTable(hashTable, m);
+	printf("Collisions: %d\n", coll);
 
 	putchar('\n');
-	printTable(hashTable, m);
 
+	printf("Quadratic probing:\n");
+	char *hashTable2 = hashOpenAddrQuadratic(line, m, &coll);
+	printTable(hashTable2, m);
+	printf("Collisions: %d\n", coll);
+
+	{
+		char c;
+		while ((c = getchar()) != '\n' && c != EOF) { }
+	}
+
+	int key;
+	printf("\n--------------\n");
+	printf("\nEnter element to find: ");
+	scanf("%c", &key);
+
+	printf("\nSearch in linear:\n");
+	searchOpenAddrLinear(key, hashTable, m);
+
+	printf("\nSearch in quadratic:\n");
+	searchOpenAddrLinear(key, hashTable2, m);
+
+
+	if (TABLE) {
+		printf("\n\n=========\n\n");
+
+		printf("|%-20s|%-20s|%13s %-27s|\n", "Table size", "Symbols", " ", "Collisions");
+		printf("|%-20s|%-20s|%-20s|%-20s|\n", " ", " ", "Linear", "Quadratic");
+		char *hashTableTest;
+		int collLinear;
+		int collQuadratic;
+		for (int tablesize = 1; tablesize < 50; ++tablesize) {
+			hashOpenAddrLinear(line, tablesize, &collLinear);	
+			hashOpenAddrQuadratic(line, tablesize, &collQuadratic);		
+			printf("|%-20d|%-20d|%-20d|%-20d|\n", tablesize, strlen(line), collLinear, collQuadratic);
+		}
+	}
+	
 	return 0;
 }
 
--- hashOpenAddr.h
+++ hashOpenAddr.h
@@ -1,10 +1,11 @@
 #ifndef HASHOPENADDR
 #define HASHOPENADDR
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char* hashOpenAddrLinear(char* line, int m)
+char* hashOpenAddrLinear(char* line, int m, int *collisions)
 {
 	char *hashTable = calloc(sizeof(char), m);
 	int length = strlen(line);
@@ -12,6 +13,7 @@ char* hashOpenAddrLinear(char* line, int m)
 	int ost;
 	int d = 1;
 	int count = 0;
+	*collisions = 0;
 	for (int i = 0; i < length; ++i) {
 		ost = (line[i] - 'a') % m;
 		while (count < m) {
@@ -21,11 +23,89 @@ char* hashOpenAddrLinear(char* line, int m)
 				++count;
 				break;
 			}
+			++(*collisions);
 			ost = ost + d;
 			if (ost >= m) ost = ost - m;
 		}
 	}
 	return hashTable;
+}
+
+char* hashOpenAddrQuadratic(char* line, int m, int *collisions)
+{
+	char *hashTable = calloc(sizeof(char), m);
+	int length = strlen(line);
+
+	int ost;
+	int d = 1;
+	int count = 0;
+	*collisions = 0;
+	for (int i = 0; i < length; ++i) {
+		ost = (line[i] - 'a') % m;
+		while (count < m) {
+			if (hashTable[ost] == line[i]) break;
+			if (hashTable[ost] == 0) {
+				hashTable[ost] = line[i];
+				++count;
+				break;
+			}
+			if (d >= m) break; 
+			++(*collisions);
+			ost = ost + d;
+			if (ost >= m) ost = ost - m;
+			d = d + 2;
+		}
+	}
+	return hashTable;
+}
+
+int searchOpenAddrLinear(char key, char* hashTable, int m)
+{
+	int ost = (key - 'a') % m;
+	int d = 1;
+	int count = 0;
+	while (count < m) {
+		if (hashTable[ost] == key) {
+			printf("Element is found.\n");
+			printf("Idx: %d\n", ost);
+			return ost;
+		}
+		if (hashTable[ost] == 0) {
+			printf("Element '%c' is not found.\n", key);
+			return -1;
+		}
+		if (d >= m) break; 
+		ost = ost + d;
+		if (ost >= m) ost = ost - m;
+		++count;
+	}
+	printf("Element '%c' is not found.\n", key);
+	return -1;
+}
+
+int searchOpenAddrQuadratic(char key, char* hashTable, int m)
+{
+	int ost = (key - 'a') % m;
+	int d = 1;
+	int count = 0;
+	while (count < m) {
+		if (hashTable[ost] == key) {
+			printf("Element is found.\n");
+			printf("Idx: %d\n", ost);
+			return ost;
+		}
+		if (hashTable[ost] == 0) {
+			printf("Element '%c' is not found.\n", key);
+			return -1;
+		}
+		if (d >= m) break; 
+		ost = ost + d;
+		d = d + 2;
+		if (ost >= m) ost = ost - m;
+		++count;
+	}
+	printf("Element '%c' is not found.\n", key);
+	return -1;
 }
 
 #endif // #ifndef HASHOPENADDR	
