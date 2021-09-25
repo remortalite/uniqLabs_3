92d7e76dce113af77c891190e09e0d1e1af46e8e
Ignat Mora <morrig407@gmail.com>
2021-09-25 13:21:55 +0700

    Fix page showing

--- cw/main.c
+++ cw/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,77 +28,8 @@ void from_866_to_utf8(char *in, size_t *sizein, char *out, size_t *sizeout) {
 	iconv(cd, &in, sizein, &out, sizeout);
 }
 
-void printRecord(Record record) {
-	printf("%-32s %-18s %-6d %-6d %-10s\n", 
-					record.person,
-					record.street,
-					record.house,
-					record.apt,
-					record.date
-					);	
-}
-
-void showPage(Record* base, int page) {
-	int idx = page;
-	int N = RECONPAGE;
-	printf("Page %d.\n", page);
-	for (int i = idx; i < (idx + N); ++i) {
-		printRecord(base[i]);
-	}
-
-	printf("\n\n\e[3mPress 'd' for next page\nPress 'a' for previous page\n\e[0m");
-}
-
-void clearScreen() {
-	system("clear");
-}
-
-void show(Record* base) {
-	char c;
-	int page = 0;
-	int N = RECONPAGE;
-	clearScreen();
-	showPage(base, page);
-	while ((c = getc(stdin)) != 'q') {
-		switch(c) {
-				case 'd':
-					clearScreen();
-					if ((page+1) <= (4000/N + (4000%N ? 1 : 0))) ++page;
-					showPage(base, page);
-					break;
-				case 'a':
-					clearScreen();
-					if ((page-1) >= 0) --page;
-					showPage(base, page);
-					break;
-		}
-	}
-}
-
-int main() {
-	FILE *fp;
-	fp = fopen("testBase4.dat", "rb");
-
-	struct record base[4000] = { 0 };
-	struct record2 base2[4000] = { 0 };
-
-	int i;
-	i = fread((struct record *) base, sizeof(struct record), 4000, fp);
-
-	/*
-	for (i = 0; i < 4; ++i) {
-		printf("'%s'='%s'='%4d' %4d %s\n",
-			base[i].person,
-			base[i].street,
-			base[i].house,
-			base[i].apt,
-			base[i].date
-			);
-	}
-	printf("--------------\n");
-	*/
-
-	for (i = 0; i < 4000; ++i) {
+void decode(struct record2* base2, struct record* base) {
+	for (int i = 0; i < 4000; ++i) {
 			size_t sizein = sizeof(base[i].person);
 			size_t sizeout = sizeof(base2[i].person);
 			char *in = calloc(sizeof(char), sizein);
@@ -124,7 +54,85 @@ int main() {
 			base2[i].house = base[i].house;
 			strcpy(base2[i].date, base[i].date);
 	}
+}
 
+void printRecord(Record record) {
+	printf("%-32s %-18s %-6d %-6d %-10s\n", 
+					record.person,
+					record.street,
+					record.house,
+					record.apt,
+					record.date
+					);	
+}
+
+void showPage(Record* base, int page) {
+	int N = RECONPAGE;
+	int idx = page*N;
+	printf("Page %d.\n", page);
+	for (int i = idx; i < (idx + N) && i < 4000; ++i) {
+		printRecord(base[i]);
+	}
+
+	printf("\n\n\e[3mPress 'd' for next page\nPress 'a' for previous page\n\e[0m");
+}
+
+void clearScreen() {
+	#ifndef ALL
+	system("clear");
+	#endif
+}
+
+void show(Record* base) {
+	char c;
+	int page = 0;
+	int N = RECONPAGE;
+	clearScreen();
+	showPage(base, page);
+	while ((c = getc(stdin)) != 'q') {
+		switch(c) {
+				case 'd':
+					clearScreen();
+					if ((page+1) < (4000/N + (4000%N ? 1 : 0))) ++page;
+					showPage(base, page);
+					break;
+				case 'a':
+					clearScreen();
+					if ((page-1) >= 0) --page;
+					showPage(base, page);
+					break;
+		}
+	}
+}
+
+int main() {
+	FILE *fp;
+	fp = fopen("testBase4.dat", "rb");
+
+	//struct record base[4000] = { 0 };
+	struct record* base = calloc(sizeof(struct record), 4000);
+	struct record2* base2 = calloc(sizeof(struct record2), 4000);
+	//struct record2* base2[4000] = { 0 };
+
+	int i = fread((struct record *) base, sizeof(struct record), 4000, fp);
+	if (i != 4000) fprintf(stderr, "Some errors with reading from .dat file\n");
+
+	fclose(fp);
+
+	/*
+	for (i = 0; i < 4; ++i) {
+		printf("'%s'='%s'='%4d' %4d %s\n",
+			base[i].person,
+			base[i].street,
+			base[i].house,
+			base[i].apt,
+			base[i].date
+			);
+	}
+	printf("--------------\n");
+	*/
+
+	decode(base2, base);
 	/*
 	for (i = 0; i < 4; ++i) {
 		printf("%s ... %s ... %4d ... %4d ... %s\n",
